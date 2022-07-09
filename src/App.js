6b263e529a9f7ec1ae8bc15391f206d7d3b0997c
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2022-07-09 22:58:16 +0300

    r

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    context: [],
+    name: '',
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  handleChange = event => {
+    this.setState({
+      name: event.currentTarget.value,
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: 'auto',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+          padding: 50,
+        }}
+      >
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Phonebook
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">add contact</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
